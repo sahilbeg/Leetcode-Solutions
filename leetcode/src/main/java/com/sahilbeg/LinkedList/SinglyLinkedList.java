9ee2d133489936ab28748a2ad87b0ae338db8a71
Sahil Beg <sahilbeg2507@gmail.com>
2023-10-23 12:22:27 +0530

    Feat: Added a method to call all the operations on linkedList

--- leetcode/src/main/java/com/sahilbeg/LinkedList/SinglyLinkedList.java
+++ leetcode/src/main/java/com/sahilbeg/LinkedList/SinglyLinkedList.java
@@ -179,4 +179,31 @@ public class SinglyLinkedList<T> {
         }
     }
 
+    //Method to call all the Linked list opertaions in the main java
+
+    public void  mainDisplay(){
+        SinglyLinkedList<Integer> list1 = new SinglyLinkedList<>();
+        list1.insert(10);
+        list1.append(20);
+        list1.insert(4);
+        list1.append(5);
+
+        System.out.println("Before reverse");
+        list1.display();
+        list1.reverseList();
+        System.out.println("After reverse");
+        list1.display();
+        list1.hasCycle();
+        list1.makeCycle();
+        
+        Node<Integer> cycleStartNode = list1.detectCycle();
+        if (cycleStartNode != null) {
+            System.out.println("Cycle detected, it starts at node with value: " + cycleStartNode.data);
+        } else {
+            System.out.println("No cycle detected.");
+        }
+
+        list1.detectCycle();
+    }
+
 }
