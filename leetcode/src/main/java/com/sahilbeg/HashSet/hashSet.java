2443664fd53bcecf44f4a8326b7b599f383115b0
Sahil Beg <sahilbeg2507@gmail.com>
2023-10-23 12:27:50 +0530

    Feat: Created a HashSet using HashSet library in java

---
+++ leetcode/src/main/java/com/sahilbeg/HashSet/hashSet.java
@@ -0,0 +1,6 @@
+package com.sahilbeg.HashSet;
+import java.util.HashSet; //importing the hashSet library
+public class hashSet {
+    
+    HashSet<Integer> set =  new HashSet<>(); // Creating a new hashSet in java
+}
