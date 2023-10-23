ee30a665a4f30dab4e63eee546aab1ecb60132c4
Sahil Beg <sahilbeg2507@gmail.com>
2023-10-23 13:55:05 +0530

    Feat: Added the following
    1. Import Iterator in HashSet
    2. Used Iterator to Iterate over the HashSet Elements

--- leetcode/src/main/java/com/sahilbeg/HashSet/Hashing.java
+++ leetcode/src/main/java/com/sahilbeg/HashSet/Hashing.java
@@ -1,5 +1,6 @@
 package com.sahilbeg.HashSet;
 import java.util.HashSet; //importing the hashSet library
+import java.util.Iterator;
 
 
 public class Hashing {
@@ -20,6 +21,12 @@ public class Hashing {
         //Print all the elements of the HashSet
         System.out.println("My Hashset is:- "+set); 
 
+        //Iterator loop to travell the hashset
+        Iterator it = set.iterator();
+
+        while(it.hasNext()){
+            System.out.println("Element using iterator:- "+it.next()); 
+        }
         // Searching values into hashSet
         if(set.contains(3)){
             System.out.println("Elemnet is present");
